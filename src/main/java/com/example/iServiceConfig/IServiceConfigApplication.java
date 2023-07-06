7a54df6bdcaeb2261d2f6b205a6937bd6be9c039
sqxsss <1514713140@qq.com>
2023-07-05 22:44:58 -0400

    Initial commit

---
+++ src/main/java/com/example/iServiceConfig/IServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.iServiceConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IServiceConfigApplication.class, args);
+	}
+
+}
